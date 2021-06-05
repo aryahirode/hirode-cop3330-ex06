039f3af11c7f4fe8577b68715d9c641f9ec785f7
Arya Hirode <aryahirode2001@gmail.com>
2021-06-05 12:57:10 -0400

    challenge1 version is working

---
+++ src/main/java/challenge01/Retirement_Calculator.java
@@ -0,0 +1,47 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Arya Hirode
+ */
+package challenge01;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class Retirement_Calculator {
+    static Scanner in = new Scanner(System.in);
+    public static void main(String[] args) {
+        Retirement_Calculator myApp = new Retirement_Calculator();
+        myApp.retirementYear();
+    }
+
+    private int agePrompt() {
+        System.out.print("What is your current age? ");
+        return Integer.parseInt(in.nextLine());
+    }
+
+    private int retirePrompt() {
+        System.out.print("At what age would you like to retire? ");
+        return Integer.parseInt(in.nextLine());
+    }
+
+    private int yearsLeft(int age, int ret) {
+        if (ret - age < 0) {
+            return ret - age;
+        }
+        System.out.println("You have " + (ret - age) + " years left until you can retire.");
+        return ret - age;
+    }
+
+    private void retirementYear() {
+        int yearsLeft = yearsLeft(agePrompt(), retirePrompt());
+        if (yearsLeft < 0) {
+            System.out.println("Congrats! You can already retire.");
+            return;
+        }
+        LocalDate yearNow = LocalDate.now();
+        LocalDate yearRet = LocalDate.now().plusYears(yearsLeft);
+        DateTimeFormatter newFormat = DateTimeFormatter.ofPattern("yyyy");
+        System.out.println("It's " + yearNow.format(newFormat) + ", so you can retire in " + yearRet.format(newFormat));
+    }
+}
